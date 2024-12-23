c406cc2730c0d96d6df46292c61a7676c4e8ccb6
Efa <efatariku07@gamil.com>
2024-12-23 13:16:40 +0300

    modified:   back/src/controllers/message.controller.js 	modified:   back/src/routes/message.route.js 	modified:   front/package-lock.json 	modified:   front/package.json 	new file:   front/postcss.config.js 	deleted:    front/src/App.css 	modified:   front/src/App.jsx 	deleted:    front/src/assets/react.svg 	modified:   front/src/index.css 	modified:   front/src/main.jsx 	new file:   front/tailwind.config.js

--- back/src/controllers/message.controller.js
+++ back/src/controllers/message.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js"
+
 
 export const getUsers =  async (req,res) => {
     try {
@@ -17,13 +19,41 @@ export const getMsg = async (req, res) => {
         const {id:userToChatId} = req.params
         const myId = req.user._id
 
-        const messages = await MessageChannel.find({
+        const messages = await Message.find({
             $or:[
                 {senderId:myId, receverId:userToChatId},
                 {senderId:userToChatId, receverId:myId}
             ]
         })
         res.status(200).json(messages)
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({message:"Internal server error"})
+    }
+}
+
+export const sendMessage = async (req, res) => {
+    try{
+        const {text,image} = req.body
+        const {id:receverId} = req.params
+        const senderId = req.user._id
+
+        let imageUrl = ""
+        if(image){
+            const uploadedImage = await cloudinary.uploader.upload(image,{
+                upload_preset:"chat_app"
+            })
+            imageUrl = uploadedImage.secure_url
+        }
+        const newMessage = new Message({
+            senderId,
+            receverId,
+            text,
+            image:imageUrl
+        })
+
+        await newMessage.save()
+        res.status(201).json(newMessage)
     } catch (error) {
         console.log(error.message)
         res.status(500).json({message:"Internal server error"})
--- back/src/routes/message.route.js
+++ back/src/routes/message.route.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { protectRoute } from "../middleware/protect.middleware.js";
-import {getUsers,getMsg} from "../controllers/message.controller.js"
+import {getUsers,getMsg,sendMessage} from "../controllers/message.controller.js"
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsers)
 router.get("/:id",protectRoute,getMsg)
+router.post("/send/:id",protectRoute,sendMessage)
 
 export default router
