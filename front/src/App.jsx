3aa801a3502f0d0a3401cb29bef5deb961b17e1b
Efa <efatariku07@gamil.com>
2024-12-25 10:50:54 +0300

    modified:   front/package-lock.json 	modified:   front/package.json 	modified:   front/src/App.jsx 	new file:   front/src/components/Navbar.jsx 	new file:   front/src/lib/axios.js 	modified:   front/src/main.jsx 	new file:   front/src/pages/Home.jsx 	new file:   front/src/pages/LoginPage.jsx 	new file:   front/src/pages/ProfilePage.jsx 	new file:   front/src/pages/Settings.jsx 	new file:   front/src/pages/SignUpPage.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
+import Settings from './pages/Settings'
+import { Routes, Route } from 'react-router-dom'
+
 
 const App = () => {
   return (
     <>
-    
+    <Navbar />
+    <Routes>
+    <Route path="/"  element={<Home />} />
+    <Route path="/signup" element={<SignUpPage />} />
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/profile" element={<ProfilePage />} />
+    <Route path="/settings" element={<Settings />} />
+    <Route path="/profile" element={<ProfilePage />} />
+    </Routes>
     </>
   )
 }
---
+++ front/src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div className='bg-slate-50 mt-5'>LoginPage</div>
+  )
+}
+
+export default LoginPage
